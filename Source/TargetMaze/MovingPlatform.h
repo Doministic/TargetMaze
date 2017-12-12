319c040d696ea471a38cf68dc148d976af21ba11
Dominic Lagle <D.Lagle444@gmail.com>
2017-12-12 08:06:24 -0800

    Platform created and moving back and forth.

--- Source/TargetMaze/MovingPlatform.h
+++ Source/TargetMaze/MovingPlatform.h
@@ -14,7 +14,21 @@ class TARGETMAZE_API AMovingPlatform : public AStaticMeshActor
 {
 	GENERATED_BODY()
 	
+private:
+	AMovingPlatform();
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+	float speed = 10.0f;
 	
-	
-	
+	UPROPERTY(EditAnywhere, meta = (MakeEditWidget = true))
+	FVector TargetLocation;
+
+
+	FVector GlobalStartLocation;
+
+	FVector GlobalTargetLocation;
 };
